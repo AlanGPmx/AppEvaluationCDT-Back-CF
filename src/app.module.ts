69571490abb38b0a7c9ee1b23580472efa3c471d
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-15 16:52:12 -0500

    feat: S agregan los Productos en el app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ProductosModule } from './components/productos/productos.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    ProductosModule
   ],
   controllers: [],
   providers: [],
