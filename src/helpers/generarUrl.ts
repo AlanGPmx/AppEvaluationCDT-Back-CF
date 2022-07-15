34537aa72fa3a1191301c77067ff93d20ff0fc23
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-15 17:08:09 -0500

    fix: Se corrigen textos en las constantes

--- src/helpers/generarUrl.ts
+++ src/helpers/generarUrl.ts
@@ -1,27 +1,40 @@
-import { AREA, CODIGO_400, CODIGO_401, CODIGO_404, CODIGO_409, CODIGO_500, ENLACE_BASE } from '../config/constantes';
+import {
+  AREA,
+  CODIGO_400,
+  CODIGO_401,
+  CODIGO_404,
+  CODIGO_409,
+  CODIGO_500,
+  ENLACE_BASE,
+} from '../config/constantes';
 
 export const generarUrl = (codigo: number, esInfo: boolean): string => {
-    let condigoInterno: string;
-    const nombreMso = 'APPEvaluationCDT';
-    
-    switch (codigo){
-        case CODIGO_400: condigoInterno = '4000';
-        break;
-    
-        case CODIGO_401: condigoInterno = '106401';
-        break;
-    
-        case CODIGO_404: condigoInterno = '4002';
-        break;
-    
-        case CODIGO_409: condigoInterno = '4003';
-        break;
-    
-        case CODIGO_500: condigoInterno = '5000';
-        break;
-    }
-  
-    return esInfo
-        ? `${ ENLACE_BASE }${ codigo }.${ AREA }-${ nombreMso }.${ condigoInterno }`
-        : `${ codigo }.${ AREA }-${ nombreMso }.${ condigoInterno }`;
-}
+  let condigoInterno: string;
+  const nombreMso = process.env['NOMBRE_MSO'];
+
+  switch (codigo) {
+    case CODIGO_400:
+      condigoInterno = '4000';
+      break;
+
+    case CODIGO_401:
+      condigoInterno = '106401';
+      break;
+
+    case CODIGO_404:
+      condigoInterno = '4002';
+      break;
+
+    case CODIGO_409:
+      condigoInterno = '4003';
+      break;
+
+    case CODIGO_500:
+      condigoInterno = '5000';
+      break;
+  }
+
+  return esInfo
+    ? `${ENLACE_BASE}${codigo}.${AREA}-${nombreMso}.${condigoInterno}`
+    : `${codigo}.${AREA}-${nombreMso}.${condigoInterno}`;
+};
